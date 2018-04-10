65572f449376af5a68f9416d661e720b708271a7
Dominik Gresch <greschd@gmx.ch>
2018-04-10 08:07:26 +0200

    Initial commit from cookiecutter template.

---
+++ setup.py
@@ -0,0 +1,48 @@
+
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import os
+import codecs
+from setuptools import setup
+
+
+def read(fname):
+    file_path = os.path.join(os.path.dirname(__file__), fname)
+    return codecs.open(file_path, encoding='utf-8').read()
+
+
+setup(
+    name='pytest-quality',
+    version='0.1.0',
+    author='Dominik Gresch',
+    author_email='greschd@gmx.ch',
+    maintainer='Dominik Gresch',
+    maintainer_email='greschd@gmx.ch',
+    license='GNU GPL v3.0',
+    url='https://github.com/greschd/pytest-quality',
+    description='A plugin to run quality (non-binary) tests with pytest.',
+    long_description=read('README.rst'),
+    py_modules=['pytest_quality'],
+    python_requires='>=3.5',
+    install_requires=['pytest>=3.1.1'],
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Framework :: Pytest',
+        'Intended Audience :: Developers',
+        'Topic :: Software Development :: Testing',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: Implementation :: CPython',
+        'Programming Language :: Python :: Implementation :: PyPy',
+        'Operating System :: OS Independent',
+        'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
+    ],
+    entry_points={
+        'pytest11': [
+            'quality = pytest_quality',
+        ],
+    },
+)
