0d2804514ebf4e4a898b7937980698103c403451
Dominik Gresch <greschd@gmx.ch>
2018-06-14 11:11:33 +0200

    Change version number to 0.0.0a1

--- pytest_score/__init__.py
+++ pytest_score/__init__.py
@@ -3,7 +3,7 @@
 Defines a pytest plugin for tests which have a 'score' in addition to a binary pass / fail result.
 """
 
-__version__ = '0.1.0a1'
+__version__ = '0.0.0a1'
 
 from ._fixtures import *
 from ._plugin import *
